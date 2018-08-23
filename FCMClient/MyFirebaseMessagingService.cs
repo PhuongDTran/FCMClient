ca1665e19fc72f031e305e15c4cd7391fefb5be1
PhuongTran <tranduyphuong2110@gmail.com>
2018-08-22 17:04:08 -0700

    Add a local notification sender

--- FCMClient/MyFirebaseMessagingService.cs
+++ FCMClient/MyFirebaseMessagingService.cs
@@ -4,6 +4,9 @@ using Android.Content;
 using Android.Media;
 using Android.Util;
 using Firebase.Messaging;
+using FCMClient;
+using System.Collections.Generic;
+using Android.Support.V4.App;
 
 namespace FCMClient
 {
@@ -15,7 +18,35 @@ namespace FCMClient
         public override void OnMessageReceived(RemoteMessage message)
         {
             Log.Debug(TAG, "From: " + message.From);
-            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+
+            var body = message.GetNotification().Body;
+            Log.Debug(TAG, "Notification Message Body: " + body);
+            SendNotification(body, message.Data);
+        }
+
+        void SendNotification(string messageBody, IDictionary<string, string> data)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            foreach (var key in data.Keys)
+            {
+                intent.PutExtra(key, data[key]);
+            }
+
+            var pendingIntent = PendingIntent.GetActivity(this,
+                                                          MainActivity.NOTIFICATION_ID,
+                                                          intent,
+                                                          PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+                                      .SetSmallIcon(Resource.Drawable.ic_stat_ic_notification)
+                                      .SetContentTitle("FCM Message")
+                                      .SetContentText(messageBody)
+                                      .SetAutoCancel(true)
+                                      .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(MainActivity.NOTIFICATION_ID, notificationBuilder.Build());
         }
     }
 }
